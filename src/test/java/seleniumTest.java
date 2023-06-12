9c167f7549992273260741d9277d2aa5690075bd
Valstion <lordvalstion@gmail.com>
2023-06-12 14:47:28 +0300

    All 5 tests

--- src/test/java/seleniumTest.java
+++ src/test/java/seleniumTest.java
@@ -1,23 +1,20 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import javax.xml.namespace.QName;
+import org.testng.annotations.*;
 import java.util.concurrent.TimeUnit;
 public class seleniumTest {
-
     public static WebDriver driver;
-
-    @Test
-    public void firstTest(){
+    public void login(){
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+    }
+    @Test
+    public void firstTest(){
+        driver.get("https://www.saucedemo.com/inventory.html");
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
         driver.findElement(By.id("checkout")).click();
@@ -27,13 +24,9 @@ public class seleniumTest {
         driver.findElement(By.id("continue")).click();
         driver.findElement(By.id("finish")).click();
     }
-
 @Test
 public void secondTest(){
-    driver.get("https://www.saucedemo.com/");
-    driver.findElement(By.id("user-name")).sendKeys("standard_user");
-    driver.findElement(By.id("password")).sendKeys("secret_sauce");
-    driver.findElement(By.id("login-button")).click();
+    driver.get("https://www.saucedemo.com/inventory.html");
     driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
     driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
     driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt")).click();
@@ -50,10 +43,7 @@ public void secondTest(){
 }
     @Test
     public void thirdTest(){// cia ketvirtas testas man atrodo. paskaityk salygas
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        driver.get("https://www.saucedemo.com/inventory.html");
         String itemName = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div")).getText();
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
@@ -70,11 +60,7 @@ public void secondTest(){
     }
     @Test
     public void forthTest(){
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
+        driver.get("https://www.saucedemo.com/inventory.html");
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         String backpack = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div")).getText();
         driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
@@ -88,7 +74,6 @@ public void secondTest(){
         driver.findElement(By.id("add-to-cart-test.allthethings()-t-shirt-(red)")).click();
         String redTshirt= driver.findElement(By.xpath("//*[@id=\"item_3_title_link\"]/div")).getText();
         driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
-
 
         String itemNameInCart1 = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div")).getText();
         Assert.assertEquals (itemNameInCart1,backpack);
@@ -107,73 +92,62 @@ public void secondTest(){
 
         String itemNameInCart6 = driver.findElement(By.xpath("//*[@id=\"item_3_title_link\"]/div")).getText();
         Assert.assertEquals (itemNameInCart6,redTshirt);
-
     }
-
 @Test
 public void fifthTest(){
-    driver.get("https://www.saucedemo.com/");
-    driver.findElement(By.id("user-name")).sendKeys("standard_user");
-    driver.findElement(By.id("password")).sendKeys("secret_sauce");
-    driver.findElement(By.id("login-button")).click();
-
-    //driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+    driver.get("https://www.saucedemo.com/inventory.html");
     String backpack1 = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div")).getText();
     driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div")).click();
     String itemNameInCart11 = driver.findElement(By.xpath("//*[@id=\"inventory_item_container\"]/div/div/div[2]/div[1]")).getText();
     Assert.assertEquals (itemNameInCart11,backpack1);
     driver.get("https://www.saucedemo.com/inventory.html");
 
-
-   // driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
     String bikeLight11= driver.findElement(By.xpath("//*[@id=\"item_0_title_link\"]/div")).getText();
     driver.findElement(By.xpath("//*[@id=\"item_0_title_link\"]/div")).click();
     String itemNameInCart12 = driver.findElement(By.xpath("//*[@id=\"inventory_item_container\"]/div/div/div[2]/div[1]")).getText();
     Assert.assertEquals (itemNameInCart12, bikeLight11);
     driver.get("https://www.saucedemo.com/inventory.html");
 
-
- //   driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt")).click();
     String boltTshirt11= driver.findElement(By.xpath("//*[@id=\"item_1_title_link\"]/div")).getText();
     driver.findElement(By.xpath("//*[@id=\"item_1_title_link\"]/div")).click();
     String itemNameInCart13 = driver.findElement(By.xpath("//*[@id=\"inventory_item_container\"]/div/div/div[2]/div[1]")).getText();
     Assert.assertEquals (itemNameInCart13, boltTshirt11);
     driver.get("https://www.saucedemo.com/inventory.html");
 
-
-
-    //driver.findElement(By.id("add-to-cart-sauce-labs-fleece-jacket")).click();
     String fleesejacket11= driver.findElement(By.xpath("//*[@id=\"item_5_title_link\"]/div")).getText();
     driver.findElement(By.xpath("//*[@id=\"item_5_title_link\"]/div")).click();
     String itemNameInCart14 = driver.findElement(By.xpath("//*[@id=\"inventory_item_container\"]/div/div/div[2]/div[1]")).getText();
     Assert.assertEquals (itemNameInCart14, fleesejacket11);
     driver.get("https://www.saucedemo.com/inventory.html");
 
-
-
     driver.findElement(By.id("add-to-cart-sauce-labs-onesie")).click();
     String labsonesize11= driver.findElement(By.xpath("//*[@id=\"item_2_title_link\"]/div")).getText();
-  //  driver.findElement(By.id("add-to-cart-test.allthethings()-t-shirt-(red)")).click();
     driver.findElement(By.xpath("//*[@id=\"item_2_title_link\"]/div")).click();
     String itemNameInCart15 = driver.findElement(By.xpath("//*[@id=\"inventory_item_container\"]/div/div/div[2]/div[1]")).getText();
     Assert.assertEquals (itemNameInCart15, labsonesize11);
     driver.get("https://www.saucedemo.com/inventory.html");
 
-   // driver.findElement(By.id("Test.allTheThings() T-Shirt (Red)e")).click();
     String redtr11= driver.findElement(By.xpath("//*[@id=\"item_3_title_link\"]/div")).getText();
     driver.findElement(By.xpath("//*[@id=\"item_3_title_link\"]/div")).click();
     String itemNameInCart16 = driver.findElement(By.xpath("//*[@id=\"inventory_item_container\"]/div/div/div[2]/div[1]")).getText();
     Assert.assertFalse (Boolean.parseBoolean(itemNameInCart16), redtr11);
     driver.get("https://www.saucedemo.com/inventory.html");
-
-
-
-
-
+}
+@AfterMethod
+public void afterMethod(){
+    driver.get("https://www.saucedemo.com/inventory.html");
+    driver.findElement(By.id("react-burger-menu-btn")).click();
+    driver.findElement(By.id("reset_sidebar_link")).click();
 }
     @BeforeClass
     public void beforeLenta(){
         driver = new ChromeDriver();;
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20L,TimeUnit.SECONDS);
+        login();
+}
+@AfterClass
+    public void afterAllTests(){
+        driver.quit();
 }
 }
